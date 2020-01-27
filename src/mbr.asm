75f43d66625bc963f484477b5db2b9e79187b5d9
jriemenschneider <jriemenschneider@luc.edu>
2020-01-27 00:55:51 +0000

    Started notes on putStr function

--- src/mbr.asm
+++ src/mbr.asm
@@ -4,17 +4,9 @@
 [BITS 16]
 
 boot: ; This file starts executing here.
-;main:
-;	mov ah, 0x0e
-;	xor bh,bh
-;	mov bl,7
-;	mov al,'N'
-;	int 16
-;loop:
-;	jmp loop
-main:
+main: ;need to edit this still from printChar
 	mov al, 'N'
-	call printChar
+	call putStr
 	move al, 'E'
 	call printChar
 	mov al, 'I'
@@ -23,20 +15,15 @@ main:
 	call printChar
 loop:
 	jmp loop
-
-;add2nums:
-;	push bp
-;	mov bp,sp
-;	mov ax,[bp+6]
-;	mov bx,[bp+4]
-;	add ax,bx
-;	pop bp
+;printChar: PRACTICE FOR PUSHING WITH GIT
+;	mov ah, 0x0e ; Below insructions set up the page so it can print al there
+;	xor bh, bh
+;	mov bl, 7
+;	int 16 call the BIOS	
 ;	ret
-printChar:
-	mov ah, 0x0e ; Below insructions set up the page so it can print al there
-	xor bh, bh
-	mov bl, 7
-	;int 16 call the BIOS? Do I need this?	
+putStr:
+	;Null terminated string is going to look like  'J''a''i''m''i'e''\0'
+	int 16 ;calls the BIOS
 	ret
     hlt
 times 510-($-$$) db 0 ; Skip to end of boot sector
