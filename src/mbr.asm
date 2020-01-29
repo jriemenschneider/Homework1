c8c02b6b4804a3303c19e93ecc2a21c54ca024eb
jriemenschneider <jriemenschneider@luc.edu>
2020-01-29 00:43:23 +0000

    mbr.asm

--- src/mbr.asm
+++ src/mbr.asm
@@ -5,28 +5,31 @@
 
 boot: ; This file starts executing here.
 main: ;need to edit this still from printChar
-	mov al, 'N'
+	mov ah, 0x0e
+	xor bh, bh
+	mov bl, 7
+	mov ax, welcomeString ;put string in ax because its a string not a character
 	call putStr
-	move al, 'E'
-	call printChar
-	mov al, 'I'
-	call printChar
-	mov al 'L'
-	call printChar
 loop:
 	jmp loop
-;printChar: PRACTICE FOR PUSHING WITH GIT
-;	mov ah, 0x0e ; Below insructions set up the page so it can print al there
-;	xor bh, bh
-;	mov bl, 7
-;	int 16 call the BIOS	
-;	ret
+
+welcomeString:
+	dp 'welcome', 0
 putStr:
 	;Null terminated string is going to look like  'J''a''i''m''i'e''\0'
-	int 16 ;calls the BIOS
+	mov ax, [bx] ;calls the BIOS
+	cmp ax, 0
+	jne moveForward	
 	ret
+;readSector:
+
+moveForward:;moves one space forward in character
+	mov ax, [bx]
+	int 0x10
+	add bx, 1
+	jmp putStr
     hlt
-times 510-($-$$) db 0 ; Skip to end of boot sector
+times 510-($-$$) db 0 ;Skip to end of boot sector
 
 db 0x55 ; Magic Numbers
 db 0xaa ; To make disk bootable
