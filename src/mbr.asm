91e8170113140395bed10e13b2ee89166f025302
jriemenschneider <jriemenschneider@luc.edu>
2020-01-30 12:59:23 -0600

    mbr.asm

--- src/mbr.asm
+++ src/mbr.asm
@@ -3,32 +3,45 @@
 
 [BITS 16]
 
-boot: ; This file starts executing here.
-main: ;need to edit this still from printChar
+boot: ;This file starts executing here.
+
+main:
 	mov ah, 0x0e
 	xor bh, bh
 	mov bl, 7
-	mov ax, welcomeString ;put string in ax because its a string not a character
+	mov bx, STRING ;put string in ax because its a string not a character
 	call putStr
-loop:
-	jmp loop
+	mov ah, 80 ;where it needs to go to start reading from the sectors?
+	call readSector
+	jmp 0x7E00 ;then jump to 7E00(from instructions)
 
-welcomeString:
-	dp 'welcome', 0
+STRING:
+	db 'welcome', 0
 putStr:
 	;Null terminated string is going to look like  'J''a''i''m''i'e''\0'
 	mov ax, [bx] ;calls the BIOS
 	cmp ax, 0
 	jne moveForward	
 	ret
-;readSector:
+readSector:
+	mov bx, 0x7E00 ;Memory adress = 0x7E00
+	mov es, bx
+	;mov bx, 0 start to read into this
+	mov ah, 2h ; BIOS read sector
+	mov al, 80 ;read 50 sectors or so
+	mov ch, 1 ;track
+	mov cl, 2 ;sector after mbr
+	mov dh, 1 ;drive head
+	mov dl, 0 ;drive you're reading
+	int 0x13 ;from Ralf's interrupt list
+	ret
 
 moveForward:;moves one space forward in character
 	mov ax, [bx]
 	int 0x10
 	add bx, 1
 	jmp putStr
-    hlt
+hlt
 times 510-($-$$) db 0 ;Skip to end of boot sector
 
 db 0x55 ; Magic Numbers
